1ffda7da3f2cc8eb6bab784a63cd9c33fc74f086
BahromkulovMuslimbek <muslimbahromkulov145@gmail.com>
2024-06-08 05:24:26 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process, Queue
+import requests #Task 6
 import os
 
 '''Task num 1'''
@@ -154,34 +155,69 @@ import os
 
 
 
-'''Task num 5'''
-def clean_special_characters(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
+# '''Task num 5'''
+# def clean_special_characters(file_path):
+#     with open(file_path, 'r', encoding='utf-8') as file:
+#         content = file.read()
     
-    cleaned_content = ''.join(char for char in content if char.isalnum() or char.isspace())
+#     cleaned_content = ''.join(char for char in content if char.isalnum() or char.isspace())
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(cleaned_content)
+#     with open(file_path, 'w', encoding='utf-8') as file:
+#         file.write(cleaned_content)
 
-    print(f"Tozalangan matn: {cleaned_content}")
+#     print(f"Tozalangan matn: {cleaned_content}")
+
+# def main():
+#     current_directory = os.getcwd()
+#     txt_files = [f for f in os.listdir(current_directory) if f.endswith('.txt')]
+    
+#     processes = []
+    
+#     for txt_file in txt_files:
+#         file_path = os.path.join(current_directory, txt_file)
+#         process = Process(target=clean_special_characters, args=(file_path,))
+#         processes.append(process)
+#         process.start()
+    
+#     for process in processes:
+#         process.join()
+
+#     print("Barcha file lar tozalandi")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+'''Task num 6'''
+
+image_urls = [
+    "https://t4.ftcdn.net/jpg/02/66/31/75/360_F_266317554_kr7DPOoM5Uty0YCeFU9nDZTt4a2LeMJF.jpg"
+]
+
+def download_image(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_name = os.path.basename(url)
+        with open(image_name, 'wb') as file:
+            file.write(response.content)
+        print(f"{image_name} yuklandi.")
+    else:
+        print(f"{url} dan rasm yuklab olishda hato!")
 
 def main():
-    current_directory = os.getcwd()
-    txt_files = [f for f in os.listdir(current_directory) if f.endswith('.txt')]
-    
     processes = []
     
-    for txt_file in txt_files:
-        file_path = os.path.join(current_directory, txt_file)
-        process = Process(target=clean_special_characters, args=(file_path,))
+    for url in image_urls:
+        process = Process(target = download_image, args = (url,))
         processes.append(process)
         process.start()
-    
+
     for process in processes:
         process.join()
 
-    print("Barcha file lar tozalandi")
+    print("Barcha rasmlar yuklandi")
 
 
 if __name__ == "__main__":
